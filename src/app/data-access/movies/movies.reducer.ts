3894526898f31f22c7e4d1d1406c1c66810051d9
thomasburleson <thomasburleson@gmail.com>
2022-04-08 15:09:18 -0500

    lab(3): use 'movieReducer()' and actions

--- src/app/data-access/movies/movies.reducer.ts
+++ src/app/data-access/movies/movies.reducer.ts
@@ -1,24 +1,56 @@
-import { MovieState } from './movies.model';
+import { produce } from 'immer';
+
+import { MovieState, MovieItem } from './movies.model';
+import { buildMatchIndicator, computeFilteredMovies } from './movies.filters';
+
+interface MovieSearchResults {
+  searchBy: string;
+  allMovies: MovieItem[];
+}
 
 export interface MovieAction {
-  type: 'searchBy' | 'clearFilter';
-  payload?: string;
+  type: 'searchBy' | 'updateFilter';
+  payload?: MovieSearchResults | string;
 }
 
-export function searchAction(searchBy: string): MovieAction {
-  return { type: 'searchBy', payload: searchBy };
+// Redux/Ngrx-like ActionCreator functions
+
+export function searchAction(
+  searchBy: string,
+  allMovies: MovieItem[]
+): MovieAction {
+  return { type: 'searchBy', payload: { searchBy, allMovies } };
 }
 
-export function clearFilterAction(): MovieAction {
-  return { type: 'clearFilter' };
+export function updateFilterAction(filterBy: string): MovieAction {
+  return { type: 'updateFilter', payload: filterBy };
 }
 
-export function moviesReducer(state: MovieState, action: MovieAction) {
-  switch (action.type) {
-    case 'searchBy':
-      return state; // fixme
-    case 'clearFilter':
-      return state; // fixme
-  }
-  return state;
+// Redux/Ngrx-like Reducer functions
+
+const isString = (source: unknown) => typeof source === 'string';
+
+export function reduceMovieAction(
+  state: MovieState,
+  action: MovieAction
+): MovieState {
+  debugger;
+  return produce(state, (draft) => {
+    switch (action.type) {
+      case 'searchBy':
+        const { searchBy, allMovies } = action.payload as MovieSearchResults;
+        draft.allMovies = allMovies;
+        draft.searchBy = searchBy;
+        break;
+      case 'updateFilter':
+        draft.filterBy = isString(action.payload)
+          ? (action.payload as string)
+          : '';
+        break;
+    }
+    const movies = computeFilteredMovies(draft);
+    const addMatchIndicators = buildMatchIndicator(draft.filterBy);
+
+    draft.filteredMovies = addMatchIndicators(movies);
+  });
 }
